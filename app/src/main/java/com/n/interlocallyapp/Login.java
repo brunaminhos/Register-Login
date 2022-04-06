ad73cd14e0ab3940a44347637dffce8d448070ef
Bruna Minhos <bruna.minhosdossantos@student.griffith.ie>
2022-04-06 16:37:40 +0100

    Register and Login version1.0

---
+++ app/src/main/java/com/n/interlocallyapp/Login.java
@@ -0,0 +1,78 @@
+package com.n.interlocallyapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Login extends AppCompatActivity {
+    EditText mEmail, mPassword;
+    Button mLoginBtn;
+    TextView mCreateBtn;
+    FirebaseAuth fAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mEmail = findViewById(R.id.Email);
+        mPassword = findViewById(R.id.password);
+        fAuth = FirebaseAuth.getInstance();
+        mLoginBtn = findViewById(R.id.loginBtn);
+        mCreateBtn = findViewById(R.id.createText);
+
+        //validate the user input
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                // if the user enter an empty value for email or password,it will display an error message
+                if (TextUtils.isEmpty(email)) {
+                    mEmail.setError("Email is Required!");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    mPassword.setError("Password is Required!");
+                    return;
+                }
+                //defining the length of password, if is less than 6 characters it will display an error message
+                if (password.length() < 6) {
+                    mPassword.setError("Password Must be equal to 6 characters");
+                    return;
+                }
+
+                //authenticate the user
+                fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Login.this, "Logged in Successfully.", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            Toast.makeText(Login.this, "Error! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+                });
+
+            }
+        });
+    }
+}
