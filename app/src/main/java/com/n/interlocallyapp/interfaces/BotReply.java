ce5ba0596b2ce076f769a6b2e3962434055d84b9
Bruna Minhos <bruna.minhosdossantos@student.griffith.ie>
2022-05-10 21:49:34 +0100

    Removed: dialogflow

--- app/src/main/java/com/n/interlocallyapp/interfaces/BotReply.java
+++
@@ -1,8 +0,0 @@
-package com.n.interlocallyapp.interfaces;
-
-import com.google.cloud.dialogflow.v2.DetectIntentResponse;
-
-public interface BotReply {
-
-    void callback(DetectIntentResponse returnResponse);
-}
